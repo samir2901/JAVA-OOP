cc72e558a0f3b4d16d2ca77270f7b09ae8bda26f
samir2901 <thunder.cse2018@gmail.com>
2020-09-09 11:01:06 +0530

    Added Lab-4

---
+++ Lab-4/Question1/src/main/java/Shape.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Samiruddin Thunder
+ */
+class Shape {
+    public String color = "red";
+    public Shape(){        
+        System.out.println("Default Constructor of Shape");        
+    }  
+    public Shape(String color){
+        this.color = color;
+    }
+    public void printDesc(){
+        System.out.println("This shape is of color " + this.color);
+    }
+}
+
+class Rectangle extends Shape{
+    public double length=0;
+    public double breadth=0;
+    
+    public Rectangle(double length, double breadth, String color){
+        super(color);
+        this.length = length;
+        this.breadth = breadth;        
+    }
+    
+    //overriding printDesc()
+    public void printDesc(){
+        double area = this.length * this.breadth;
+        System.out.println("This rectangle shape has a length of " + this.length + ", breadth of " + this.breadth + ", area of " + area + ", color of " + this.color + ".");
+        
+    }    
+}
+
+class Circle extends Shape{
+    public double radius=0;
+        
+    public Circle(double radius,String color){
+        super(color);
+        this.radius = radius;
+    }
+    
+    //overriding printDesc()
+    public void printDesc(){
+        double area = 3.14 * this.radius * this.radius;
+        System.out.println("The circle has a radius of " + this.radius + ", area of " + area + ", color of " + this.color + ".");        
+    }    
+}
+
+class Square extends Shape{
+    public double side=0;
+        
+    public Square(double side,String color){
+        super(color);
+        this.side = side;
+    }
+    
+    //overriding printDesc()
+    public void printDesc(){
+        double area = this.side * this.side;
+        System.out.println("The square has a side of " + this.side + ", area of " + area + ", color of " + this.color + ".");        
+    }    
+}
---
+++ Lab-4/Question1/src/main/java/ShapeManager.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Samiruddin Thunder
+ */
+import java.util.*;
+
+public class ShapeManager {
+    public void manageShape(Shape shape){
+        shape.printDesc();
+    }    
+    
+    public static void main(String[] args){
+        ShapeManager sm = new ShapeManager();
+        
+        Scanner sc = new Scanner(System.in);                
+        System.out.println("Enter color: ");
+        String color = sc.next();
+        
+        Shape shape = new Shape(color);
+        sm.manageShape(shape);
+        
+        System.out.println("Enter length: ");
+        double length = Double.parseDouble(sc.next());
+        System.out.println("Enter breadth: ");
+        double breadth = Double.parseDouble(sc.next());
+        System.out.println("Enter color: ");
+        String colorRect = sc.next();
+        
+        Rectangle rect = new Rectangle(length, breadth, colorRect);
+        rect.printDesc();
+        
+        System.out.println("Enter side: ");
+        double side = Double.parseDouble(sc.next());        
+        System.out.println("Enter color: ");
+        String colorSq = sc.next();
+        
+        Square sq = new Square(side,colorSq);
+        sq.printDesc();
+        
+        System.out.println("Enter radius: ");
+        double radius = Double.parseDouble(sc.next());        
+        System.out.println("Enter color: ");
+        String colorCircle = sc.next();
+        
+        Circle c = new Circle(radius, colorCircle);
+        c.printDesc();
+    }    
+}
